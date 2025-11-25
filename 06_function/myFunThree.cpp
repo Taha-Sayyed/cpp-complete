35ac97f094a872ba1a8f8cbd2a2656501982922f
Taha Sayyed <codetaha786@gmail.com>
2025-11-25 21:06:51 +0530

    Update myFunThree.cpp

--- 06_function/myFunThree.cpp
+++ 06_function/myFunThree.cpp
@@ -1,9 +1,11 @@
+//pass by reference demostration 
+
 #include <iostream>
 
 using namespace std;
 
 int globalChaiStock = 100;
-void pourChai(int &cups){
+void pourChai(int &cups){ // Here we passes the memory reference of the 'cups'. We did not allocated in dynamic memory
     int test = 9;
     cups = cups + 5;
     
