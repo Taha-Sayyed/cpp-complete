c19790d558ab0ffaee3754648a0a5b8d0eecf264
Taha Sayyed <codetaha786@gmail.com>
2025-11-27 10:40:32 +0530

    Add static member func

---
+++ Oops-Detail/Member Function/static_member_func.cpp
@@ -0,0 +1,35 @@
+// Write a C++ program that demonstrates the use of static data members and static member functions within a class. Create a class named Student that contains a private static integer variable used to keep track of how many Student objects have been created. Initialize this static variable outside the class definition. Inside the class, define a constructor that increments the static counter each time a new Student object is instantiated. Also include a static member function that returns the current value of the counter; this function should only access static members. In the main() function, first display the initial count by calling the static function without creating any objects. Then create three Student objects, and again call the static function—without using an object—to display the total number of Student instances created. Use appropriate cout statements to show the output.
+
+
+#include<iostream>
+using namespace std;
+
+
+class Student{
+    private:
+        static int count;
+
+    public:
+        Student(){
+            count++;
+        }
+
+        static int getCount(){
+            return count;
+        }
+};
+
+
+int Student::count=0;
+
+int main(){
+
+    cout<<"Number of student currently "<<Student::getCount()<<endl;
+
+    Student s1,s2,s3;
+
+    cout<<"Now the number of students are "<<Student::getCount()<<endl;
+
+
+    return 0;
+}
