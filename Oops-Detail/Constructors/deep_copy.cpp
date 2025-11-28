5bd67663c7768536fd1c3715cc161b6680f80655
Taha Sayyed <codetaha786@gmail.com>
2025-11-28 10:57:28 +0530

    Add necessary comment

--- Oops-Detail/Constructors/deep_copy.cpp
+++ Oops-Detail/Constructors/deep_copy.cpp
@@ -1,5 +1,6 @@
 // Write a C++ program that defines a Car class capable of storing a car’s name and a list of its available colors. The class should include a constructor for initializing these values and a destructor that prints a message when an object is destroyed. In the main() function, create an instance of the Car class (for example, a Honda with two initial colors) and then perform a deep copy of this object. After copying, modify the color list of the copied object by adding an additional color. Finally, display the colors of both the deep-copied object and the original object to demonstrate that modifying the copied object does not affect the original.
 
+//No double deletion of the same memory
 
 #include<iostream>
 #include<vector>
