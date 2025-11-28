bf8f63d0a1a2eca9f0504b07823cf4818d82f8af
Taha Sayyed <codetaha786@gmail.com>
2025-11-28 10:46:19 +0530

    Add deep copy

---
+++ Oops-Detail/Constructors/deep_copy.cpp
@@ -0,0 +1,41 @@
+// Write a C++ program that defines a Car class capable of storing a car’s name and a list of its available colors. The class should include a constructor for initializing these values and a destructor that prints a message when an object is destroyed. In the main() function, create an instance of the Car class (for example, a Honda with two initial colors) and then perform a deep copy of this object. After copying, modify the color list of the copied object by adding an additional color. Finally, display the colors of both the deep-copied object and the original object to demonstrate that modifying the copied object does not affect the original.
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Car{
+    public:
+        string carName;
+        vector<string>carColor;
+
+        Car(string name,vector<string>color){
+            carName=name;
+            carColor=color;
+        }
+        ~Car(){
+            cout<<"\nDestructor Called"<<endl;
+        }
+};
+
+int main(){
+
+    Car hondaCar("Honda Car",{"Red","Blue"});
+    Car copiedObject=hondaCar;
+
+    copiedObject.carColor={"Green","Red","Blue"};
+
+    cout<<"\n-----------Original Object color--------------"<<endl;
+    for(string i:hondaCar.carColor){
+        cout<<i<<" ";
+    }
+
+    cout<<"\n----------- Copied Object Color---------------"<<endl;
+    for (string j:copiedObject.carColor){
+        cout<<j<<" ";
+    }
+
+    return 0;
+}
