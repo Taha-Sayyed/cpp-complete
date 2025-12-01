adc1334ccc5616007e1f34ad84eb01131f13a85c
Taha Sayyed <codetaha786@gmail.com>
2025-12-01 10:43:18 +0530

    Refernce to abstract class type

---
+++ Oops-Detail/Keywords/abstract_1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+    public:
+        Base(){
+            cout<<"Base constructor is called"<<endl;
+        }
+        virtual void displayDetail()=0;
+        
+        virtual ~Base(){
+            cout<<"Base Destructor is called"<<endl;
+        }
+};
+
+class Derived:public Base{
+    public:
+        Derived(){
+            cout<<"Derived Constructor is called"<<endl;
+        }
+
+        void displayDetail() override{
+            cout<<"Derived class Detail"<<endl;
+        }
+
+        ~Derived(){
+            cout<<"Derived Destructor is called"<<endl;
+        }
+};
+
+int main(){
+    
+    Derived d;
+
+    Base& ref=d;
+    ref.displayDetail();
+     
+    return 0;
+}
