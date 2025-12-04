44211f457509fe761ebe89b6aa69ba9d7467e8f3
Taha Sayyed <codetaha786@gmail.com>
2025-12-04 09:33:46 +0530

    Updated

--- Oops-Detail/Keywords/const_1.cpp
+++ Oops-Detail/Keywords/const_1.cpp
@@ -12,6 +12,7 @@ int main(){
     char* yourbuf=&that_char;
 
     char* const aptr=mybuf;
+    //*aptr='t'; //This is possible. For detail, go to notes
     //aptr=yourbuf; //Give error since aptr pointer is const
 
     return 0;
