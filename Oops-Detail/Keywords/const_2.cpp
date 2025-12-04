ec24d53907cd42aec62bd9ae4e050f8671227fca
Taha Sayyed <codetaha786@gmail.com>
2025-12-04 09:20:43 +0530

    Add cosnt-2

---
+++ Oops-Detail/Keywords/const_2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    char this_char='a';
+    char that_char='b';
+
+    const char* mybuf=&this_char;
+    char* yourbuf=&that_char;
+
+    //char* bptr=mybuf; //Give error. Becz Pointer to variable declared as `const` 
+                      //can be assigned to a pointer that is also declared as `const`
+
+    const char* bptr=mybuf;
+
+    return 0;
+}
